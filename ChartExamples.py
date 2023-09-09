6e4ff4c2b7399384feb3f134db7e4bd732fa7aa5
BogdanovSergeyA <144524443+BogdanovSergeyA@users.noreply.github.com>
2023-09-09 19:54:18 +0600

    Add files via upload
    Добавление кнопки закрытия программы.

--- ChartExamples.py
+++ ChartExamples.py
@@ -7,5 +7,17 @@ window=tk.Tk()
 window.geometry("450x450")
 window.title("Примеры построения графиков")
  
+#Функция закрытия программы
+
+def do_close():
+    window.destroy()   
+ 
+ 
+#Добавление кнопки закратия программы
+btnClose = tk.Button(window, text="Закрыть", font=('Helvetica', 10, 'bold'), command=do_close)
+btnClose.place(x=330, y=400, width=90, height=30)
+
 #запуск цикла mainloop
 window.mainloop()
+
+ 
