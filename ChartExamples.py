0e3a3bedf59458f2bc047455301e0eda34f640aa
BogdanovSergeyA <bogdanovsergey07@gmail.com>
2023-09-09 15:47:00 +0600

    Create ChartExamples.py
    Создание основы программы.

---
+++ ChartExamples.py
@@ -0,0 +1,12 @@
+#Примеры построения графиков
+
+import tkinter as tk
+
+#создание главного окна 
+window=tk.Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+ 
+#запуск цикла mainloop
+window.mainloop()
+
